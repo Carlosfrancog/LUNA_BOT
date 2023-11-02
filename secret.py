14b46a3e39e727f770ca1ea475b175ee7500cfb4
Carlosfrancog <carlosfranco.contato@gmail.com>
2023-11-02 14:50:02 -0300

    configs

--- secret.py
+++
@@ -1,5 +0,0 @@
-def Discord_token():
-    return "MTEwMDI3MTc4OTQwMzI5NTc2NQ.G8O8f6.JyvwDVEDpSqrMNlht__ep2cBKjBxHQLraftdT4"
-
-def OpenaiKey():
-    return "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"

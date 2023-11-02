dbe1e2c01b550685a9420468cd51bef8dfac1406
Carlosfrancog <87143257+Carlosfrancog@users.noreply.github.com>
2023-11-02 14:44:58 -0300

    Delete secret.py

--- secret.py
+++
@@ -1,5 +0,0 @@
-def Discord_token():
-    return "MTEwMDI3MTc4OTQwMzI5NTc2NQ.GDdA3b.iZ7giTI6-KfLSLuEU9fOlMa82ATUsAfdI5WTtc"
-
-def OpenaiKey():
-    return "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"

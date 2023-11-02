35745e260a9b6d86ef2480c2013e423c522f0565
Carlosfrancog <carlosfranco.contato@gmail.com>
2023-11-02 12:42:01 -0300

    update files

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-from requirements import *
+import os
+import discord
+from discord.ext import commands
+from discord.utils import get
+import openai
+import time
+import datetime
+from time import sleep
+import json
+import asyncio
+import requests
+import random
 from key import *
+
+intents = discord.Intents.default()
+intents.guilds = True
+intents.messages = True
+intents.message_content = True
+intents.members = True
+intents.bans = True
+intents.emojis = True
+intents.integrations = True
+intents.webhooks = True
+intents.invites = True
+intents.voice_states = True
+intents.presences = True
+intents.reactions = True
+intents.typing = True
 
 wallet_file = os.path.join(os.path.dirname(__file__), 'wallet.json')
 
